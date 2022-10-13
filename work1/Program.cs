8ec29347d6ef73693f06ec373a438457c72d13ee
Elena <lenalosyakova@gmail.com>
2022-10-13 21:40:49 +0300

    Show Array

---
+++ work1/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] array = {0, 1, 1, 1, 0, 1, 0, 0};
+int count = array.Length;
+for ( int i = 0; i < count; i++)
+{
+    Console.Write(array[i] + " ");
+}
+    
+// Задать массив из 8 элементов, заполненных нулями и единицами, вывести их на экран
