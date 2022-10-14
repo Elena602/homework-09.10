f392c41bba95c8e7498e27e008002fe65ba2651c
Elena <lenalosyakova@gmail.com>
2022-10-14 20:41:03 +0300

    pairs of numbers

---
+++ work6/Program.cs
@@ -0,0 +1,30 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+void FillArray(int[] collection)
+{
+    for (int index = 0; index < collection.Length; index++)
+    {
+   Random losk = new Random();
+   collection[index] = losk.Next(-10,20);
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    for (int index = 0; index < collection.Length; index++)
+    {
+    Console.Write($"{collection[index]} ");
+    }
+}
+
+int[] collection = new int[20];
+FillArray(collection);
+Console.Write("Массив: ");
+PrintArray(collection);
+Console.WriteLine(" ");
+int result = 0;
+for (int index = 0; index < collection.Length/2; index++)
+  {
+    result = collection[index] * collection[collection.Length - 1 - index];
+    Console.WriteLine($"Произведение пар чисел: " + result);
+  }
+ 
